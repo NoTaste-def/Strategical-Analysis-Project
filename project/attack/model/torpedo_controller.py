dc020a7f7b6c820274cd8058b278aa35d7dbe666
SOJA_Dev <rudcksdid@naver.com>
2025-06-07 11:12:05 +0900

    updated

--- project/attack/model/torpedo_controller.py
+++ project/attack/model/torpedo_controller.py
@@ -18,6 +18,9 @@ class TorpedoCommandControl(BehaviorModel):
         self.insert_output_port("target")
         self.threat_list = []
 
+        self.prev_target_positions = {}
+
+
     def ext_trans(self,port, msg):
         if port == "threat_list":
             print(f"{self.get_name()}[threat_list]: {datetime.datetime.now()}")
@@ -29,6 +32,7 @@ class TorpedoCommandControl(BehaviorModel):
         
         for t in self.threat_list:
             target =  self.platform.co.get_target(self.platform.mo, t)
+
                 
         # house keeping
         self.threat_list = []
@@ -40,7 +44,46 @@ class TorpedoCommandControl(BehaviorModel):
             msg.insert_message(message)
         
         return msg
-        
+    # def output(self, msg):
+    #     target = None
+
+    #     for t in self.threat_list:
+    #         cur_pos = self.platform.co.get_target(self.platform.mo, t)
+    #         if not cur_pos:
+    #             continue
+
+    #         prev_pos = self.prev_target_positions.get(t)
+
+    #         if prev_pos:
+    #             x_self, y_self, _ = self.platform.mo.get_position()
+    #             x_prev, y_prev, _ = prev_pos
+    #             x_cur, y_cur, _ = cur_pos
+
+    #             dx_prev = x_prev - x_self
+    #             dy_prev = y_prev - y_self
+    #             dist_prev = dx_prev**2 + dy_prev**2
+
+    #             dx_cur = x_cur - x_self
+    #             dy_cur = y_cur - y_self
+    #             dist_cur = dx_cur**2 + dy_cur**2
+
+    #             if dist_cur > dist_prev:
+    #                 target = cur_pos
+    #                 break
+
+            # # 이전 좌표 업데이트
+            # self.prev_target_positions[t] = cur_pos
+
+        # self.threat_list = []
+        # self.platform.co.reset_target()
+
+        # if target:
+        #     message = SysMessage(self.get_name(), "target")
+        #     message.insert(target)
+        #     msg.insert_message(message)
+
+        # return msg
+    
     def int_trans(self):
         if self._cur_state == "Decision":
             self._cur_state = "Wait"
--- project/defense/model/launcher.py
+++ project/defense/model/launcher.py
@@ -1,5 +1,6 @@
 import datetime
 import math
+import random
 
 from pyjevsim import BehaviorModel, Infinite
 from utils.object_db import ObjectDB
@@ -12,9 +13,9 @@ from ..mobject.self_propelled_decoy_object import SelfPropelledDecoyObject
 class Launcher(BehaviorModel):
     def __init__(self, name, platform):
         BehaviorModel.__init__(self, name)
-        
+
         self.platform = platform
-        
+
         self.init_state("Wait")
         self.insert_state("Wait", Infinite)
         self.insert_state("Launch", 0)
@@ -23,7 +24,7 @@ class Launcher(BehaviorModel):
 
         self.launch_flag = False
 
-    def ext_trans(self,port, msg):
+    def ext_trans(self, port, msg):
         if port == "order":
             print(f"{self.get_name()}[order_recv]: {datetime.datetime.now()}")
             self._cur_state = "Launch"
@@ -31,9 +32,18 @@ class Launcher(BehaviorModel):
     def output(self, msg):
         if not self.launch_flag:
             se = ObjectDB().get_executor()
+            decoy_list = self.platform.lo.get_decoy_list()
 
-            for idx, decoy in enumerate(self.platform.lo.get_decoy_list()):
+            # Surface ship 방향 가져오기 (현재 heading 추정치)
+            surface_heading = getattr(self.platform, "heading", 0)
+
+            for idx, decoy in enumerate(decoy_list):
                 destroy_t = math.ceil(decoy['lifespan'])
+
+                # 하나의 decoy만 surface ship 방향을 따라가게 설정
+                if idx == 0 and "heading" in decoy:
+                    decoy["heading"] = surface_heading  # surface ship과 동일하게 설정
+
                 if decoy["type"] == "stationary":
                     sdo = StationaryDecoyObject(self.platform.get_position(), decoy)
                     decoy_model = StationaryDecoy(f"[Decoy][{idx}]", sdo)
@@ -42,16 +52,16 @@ class Launcher(BehaviorModel):
                     decoy_model = SelfPropelledDecoy(f"[Decoy][{idx}]", sdo)
                 else:
                     sdo = None
-                
+
                 ObjectDB().decoys.append((f"[Decoy][{idx}]", sdo))
                 ObjectDB().items.append(sdo)
                 se.register_entity(decoy_model, 0, destroy_t)
-                
-        self.launch_flag = True
 
-        #se.register_entity()
+            self.launch_flag = True
+
         return None
-        
+
+
     def int_trans(self):
         if self._cur_state == "Launch":
             self._cur_state = "Wait"
